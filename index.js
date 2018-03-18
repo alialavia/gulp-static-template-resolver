67fb579c1ff63ebba4498c7adcc70beae744669f
Ali Alavi <alialavia@gmail.com>
2018-03-18 19:41:10 -0400

    Changed the plugin to accept dataFile and tagFile.

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-'user strict';
 
 var template = require('es6-template-strings');
 var through = require('through2');
 var PluginError = require('plugin-error');
-
+var reload = require('require-reload')(require);
+// Compile template files
+var fs = require('fs');
+var resolve = require('path').resolve;
 var PLUGIN_NAME = 'gulp-es6-template-resolver';
 
-/**
- * Go through files and apply templates, based on values 
- * and tags defined in tagFile and dataFile.
- * @param {tagFile} A javascript module used for defining tags
- * @param {dataFile} A JSON file containing static values used in the website
- * @param {enc} Encryption used for dataFile, default to 'utf8'
- */
+var helpers = {
+    many: function many(ui, args) {
+        //console.log({ui, args});
+        var placeholders = [].slice.call(arguments, 1);
+        //console.log({placeholders});
+        var result = placeholders.map((x) => ui(...x));
+        return result.reduce((a, b) => a + b);
 
- module.exports = function(resolverObject) {
+    }
+}
+
+/**
+- * Go through files and apply templates, based on values
+- * and tags defined in tagFile and dataFile.
+- * @param {tagFile} A javascript module used for defining tags
+- * @param {dataFile} A JSON file containing static values used in the website
+- * @param {enc} Encryption used for dataFile, default to 'utf8'
+- */
+
+module.exports = function(tagFile, dataFile, enc='utf8') {
     return through.obj(function(file, enc, callback) {
+        var state = JSON.parse(fs.readFileSync(resolve(dataFile), enc));
+        var tags = reload(resolve(tagFile));
+        var resolverObject = Object.assign({}, helpers, tags, state);
         if (file.isStream())
             return callback(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+
         try {
-            var contents = template(file.contents.toString(), resolverObject)
+            var contents = template(file.contents.toString(), resolverObject, { partial: true })
         } catch (err) {
             return callback(new PluginError(PLUGIN_NAME, err));
         }
--- test/test.js
+++ test/test.js
@@ -2,31 +2,26 @@
 
 var expect = require('chai').expect;
 var File = require('vinyl');
- 
+var fs = require('fs');
 describe('#templateResolver', function() {
+  var content = fs.readFileSync("test/index.html", "utf8");
 	it('should throw error on bad state.jsons', function() {
       // create the fake file
-      var fakeFile = new File({
-        contents: new Buffer('abufferwiththiscontent')
-      });
+      var fakeFile = new File({ contents: new Buffer(content) });
       var template_resolver = require('../')('./test/defs.js', './test/state.bad.json');
       expect(() => template_resolver.write(fakeFile)).to.throw();
     });
     
-	it('should throw error on bad state.jsons', function() {
+	it('should throw no error on good state.jsons', function() {
       // create the fake file
-      var fakeFile = new File({
-        contents: new Buffer('abufferwiththiscontent')
-      });
-      var template_resolver = require('../')('./test/defs.js', './test/state.bad.json');
-      expect(() => template_resolver.write(fakeFile)).to.throw();
+      var fakeFile = new File({ contents: new Buffer(content) });
+      var template_resolver = require('../')('./test/defs.js', './test/state.json');
+      expect(() => template_resolver.write(fakeFile)).to.not.throw();
     });
 
-    it('should throw no error on partial state jsons', function() {
+  it('should throw no error on partial state jsons', function() {
       // create the fake file
-      var fakeFile = new File({
-        contents: new Buffer('abufferwiththiscontent')
-      });
+      var fakeFile = new File({ contents: new Buffer(content) });
       var template_resolver = require('../')('./test/defs.js', './test/state.partial.json');
       expect(() => template_resolver.write(fakeFile)).to.not.throw();
     });
